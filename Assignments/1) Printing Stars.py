cb5046cf52eac7406f339fe9730206f25bc0995c
farhatrkhalil <khalilrfarhat297@gmail.com>
2025-09-23 12:02:47 +0300

    Add initial implementation of star printing pattern using a while loop

---
+++ Assignments/1) Printing Stars.py
@@ -0,0 +1,12 @@
+# Write a Python program that prints the following pattern using a while loop:
+# *
+# **
+# ***
+# ****
+# *****
+# -------------------------------------------------------------------------------------
+
+i=1
+while(i<6):
+  print("*"*i)
+  i += 1
