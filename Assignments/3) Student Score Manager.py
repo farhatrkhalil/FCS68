c7f351012e0c92a7ba9f8b07a99ae8947c4eb8b6
farhatrkhalil <khalilrfarhat297@gmail.com>
2025-10-06 22:21:06 +0300

    Refactor student score manager to initialize student names and failed students lists, and improve output formatting for scores and student details.

--- Assignments/3) Student Score Manager.py
+++ Assignments/3) Student Score Manager.py
@@ -6,7 +6,9 @@ while students_number_input <= 0:
     students_number_input = int(input("Enter number of students: "))
 
 if students_number_input > 0:
+    students_names = []
     students_scores = []
+    Failed_students = []
     for i in range(students_number_input):
 
         name_input = input(f"Enter student {i + 1} name: ")
@@ -17,17 +19,28 @@ if students_number_input > 0:
             print("Invalid input. Please enter a valid name (alphabetic characters only).")
             # Without the f, Python would print the curly braces literally instead of replacing them with variable values.
             name_input = input(f"Enter student {i + 1} name: ")
+        students_names.append(name_input)
 
         score_input = float(input(f"Enter {name_input}'s score: "))
         while score_input < 0 or score_input > 100:
             print("Invalid input. Please enter a score between 0 and 100.")
             score_input = float(input(f"Enter {name_input}'s score: "))
+        if score_input < 60:
+            Failed_students.append(name_input)
+
         students_scores.append(score_input)
 
     average_score = sum(students_scores) / students_number_input
     highest_score = max(students_scores)
-    lowest_score = min(students_scores)
+    top_index = students_scores.index(highest_score)
+    highest_score_student_name = students_names[top_index]
 
-    print(f"\nAverage Score: {average_score:.2f}")
-    print(f"Highest Score: {highest_score:.2f}")
-    print(f"Lowest Score: {lowest_score:.2f}")
+    print("\nAll students and scores:")
+    for i in range(students_number_input):
+        print(f"{students_names[i]} - {students_scores[i]}")
+
+    # 2f means “floating-point number with 0 or 1 or 2 digits after the decimal point.”
+    print(f"\nAverage score: {average_score:.1f}")
+    print(f"Top student: {highest_score_student_name} ({highest_score:.0f})")
+    print(f"Failed students: {', '.join(Failed_students)}")
+
