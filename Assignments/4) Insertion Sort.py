404cfcfefd4d4fa33d2c2371b4ebb7e249909884
farhatrkhalil <khalilrfarhat297@gmail.com>
2025-10-18 13:44:00 +0300

    Clarify comment in insertion_sort function to specify that the last index of the sorted portion starts comparing backwards

--- Assignments/4) Insertion Sort.py
+++ Assignments/4) Insertion Sort.py
@@ -12,7 +12,7 @@ def insertion_sort(arr):
     # Traverse through 1 to len(arr)
     for i in range(1, len(arr)): #Because we start sorting from the second element in the list (index 1).
         key = arr[i]  # The current element to be inserted
-        j = i - 1     # The last index of the sorted portion
+        j = i - 1     # The last index of the sorted portion (start comparing backwards)
 
         # Move elements of arr[0..i-1], that are greater than key,
         # to one position ahead of their current position
